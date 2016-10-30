3e0aff894cd45de9bc37019d02d4e818b4af5638
TevinPls <tevinexpert@gmail.com>
2016-10-30 20:47:15 -0200

    outros Editar pronto

--- BLL/alterarAssistente.php
+++ BLL/alterarAssistente.php
@@ -7,11 +7,10 @@ $tratamento = array(".","-","/",")","(");
 $id = $_GET['id'];
 $nome = $_GET['nome'];
 $cpf = $_GET['cpf'];
-$email = $_GET['email'];
+$email = $_GET['id'];
 $nascimento = str_replace($tratamento, "", $_GET['nascimento']);
 $rg = $_GET['rg'];
 $telefone = $_GET['telefone'];
-$tipo_deficiencia = $_GET['tipo_deficiencia'];
 $cidade = $_GET['cidade'];
 $bairro = $_GET['bairro'];
 $logradouro = $_GET['logradouro'];
@@ -19,11 +18,15 @@ $complemento = $_GET['complemento'];
 $numero = $_GET['numero'];
 $uf = $_GET['uf'];
 $cep = $_GET['cep'];
+$certificado = $_GET['certificado'];
+$tipo = $_GET['tipo'];
 
-$sql = "UPDATE cliente SET nome = '$nome', cpf = '$cpf', email = '$email', nascimento = '$nascimento', rg = '$rg', 
-				telefone = '$telefone', tipo_deficiencia = '$tipo_deficiencia', cidade = '$cidade', Bairro = '$bairro',
+$sql = "UPDATE assistente SET nome = '$nome', cpf = '$cpf', email = '$email', nascimento = '$nascimento', rg = '$rg', 
+				telefone = '$telefone', cidade = '$cidade', Bairro = '$bairro', certificado = '$certificado', tipo = '$tipo',
 				Logradouro = '$logradouro', complemento = '$complemento', numero = '$numero', uf = '$uf', cep = '$cep' 
 				WHERE id = $id";
+
+				print_r($sql);
 
 if (mysqli_query($bd->conexaobd, $sql)) {
 	echo json_encode("ok");
